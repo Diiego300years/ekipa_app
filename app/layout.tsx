660dd7c2132201c0bde02ca35ffd530bbcc44198
Diiego300years <marcin.buczak@onet.eu>
2026-05-05 18:50:54 +0200

    fix: added server place

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const preferredRegion = "lhr1";
+
 export const metadata: Metadata = {
   title: "Ekipa",
   description: "Mobilny kalendarz pomysłów dla ekipy.",
